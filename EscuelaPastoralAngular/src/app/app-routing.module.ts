edcc0c2fe54477c4a8d217aaed32591474b2348d
AppDevSYS <106793712+JoelMaldonado@users.noreply.github.com>
2023-08-22 12:26:49 -0500

    avances en web

--- EscuelaPastoralAngular/src/app/app-routing.module.ts
+++ EscuelaPastoralAngular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { VerAlumnosComponent } from './components/ver-alumnos/ver-alumnos.component';
+import { VerMaestrosComponent } from './components/ver-maestros/ver-maestros.component';
+import { VerCursosComponent } from './components/ver-cursos/ver-cursos.component';
+import { VerEventosComponent } from './components/ver-eventos/ver-eventos.component';
+
+const routes: Routes = [
+  { path: 'alumno', component: VerAlumnosComponent },
+  { path: 'maestro', component: VerMaestrosComponent },
+  { path: 'curso', component: VerCursosComponent },
+  { path: 'evento', component: VerEventosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ EscuelaPastoralAngular/src/app/components/ver-maestros/ver-maestros.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Alumno } from 'src/app/interfaces/alumno';
+
+@Component({
+  selector: 'app-ver-maestros',
+  templateUrl: './ver-maestros.component.html',
+  styleUrls: ['./ver-maestros.component.css']
+})
+export class VerMaestrosComponent {
+  displayedColumns: string[] = ['id', 'nombre', 'edad', 'genero', 'acciones'];
+  dataSource = new MatTableDataSource<Alumno>(ALUMNOS);
+
+  agregarAlumno() {
+    const nuevoId = this.dataSource.data.length + 1;
+    const nuevoAlumno: Alumno = {
+      id: nuevoId,
+      nombre: `Nuevo Alumno ${nuevoId}`,
+      edad: Math.floor(Math.random() * 10) + 18,
+      genero: Math.random() > 0.5 ? 'Masculino' : 'Femenino'
+    };
+    this.dataSource.data.push(nuevoAlumno);
+    this.dataSource._updateChangeSubscription();
+  }
+
+}
+
+const ALUMNOS: Alumno[] = [
+  { id: 1, nombre: 'Juan Pérez', edad: 20, genero: 'Masculino' },
+  { id: 2, nombre: 'María Gómez', edad: 22, genero: 'Femenino' },
+  { id: 3, nombre: 'Carlos Rodríguez', edad: 21, genero: 'Masculino' },
+  // Agrega más alumnos aquí
+];
