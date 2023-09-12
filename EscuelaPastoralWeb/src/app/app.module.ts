418d99c28f44cbb68e11a1502dfd1ef782913399
AppDevSYS <106793712+JoelMaldonado@users.noreply.github.com>
2023-09-11 19:39:39 -0500

    implementando forms y login

--- EscuelaPastoralWeb/src/app/app.module.ts
+++ EscuelaPastoralWeb/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectComponent } from './ui/components/select/select.component';
+import { MenuComponent } from './ui/pages/menu/menu.component';
+import { LoginComponent } from './ui/pages/login/login.component';
+import { FormularioComponent } from './ui/pages/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectComponent,
+    MenuComponent,
+    LoginComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
